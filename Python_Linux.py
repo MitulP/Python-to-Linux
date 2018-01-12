7f0802e09f05856ce4accda56b63e41b8f9e516f
Mitul Panchal <mitul.panchal72@gmail.com>
2018-01-12 11:42:49 +0530

    Create Python_Linux.py

---
+++ Python_Linux.py
@@ -0,0 +1,9 @@
+#! /usr/bin/python
+
+#import module 
+
+from shell_command import shell_call
+shell_call("ls *.py")
+
+#Below link is contains all the shell command which you may want to execute through this program
+#https://ss64.com/bash/
